0867060a7b2c8b60202e450c0bb5903bdc93f2ac
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-02 18:18:04 -0300

    feat: Implementado métodos de Saque (`Withdraw`) e Depósito (`Deposit`).

--- ContaCorrente.ConsoleApp/Entities/CurrentAccount.cs
+++ ContaCorrente.ConsoleApp/Entities/CurrentAccount.cs
@@ -5,4 +5,27 @@ internal class CurrentAccount
     public int AccountNumber;
     public decimal AvailableBalance;
     public decimal OverdraftLimit;
+
+    public void Withdraw(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("O valor do saque precisa ser positivo.");
+            return;
+        }
+        if (amount > AvailableBalance)
+        {
+            Console.WriteLine("Você não tem esse valor disponível em conta.");
+            return;
+        }
+        AvailableBalance -= amount;
+    }
+    public void Deposit(decimal amount)
+    {
+        if (amount <= 0)
+        {
+            Console.WriteLine("O valor para depósito precisa ser positivo.");
+        }
+        AvailableBalance += amount;
+    }
 }
