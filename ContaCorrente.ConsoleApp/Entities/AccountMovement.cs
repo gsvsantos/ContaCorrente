fade7c3a36c104dda1de3978d3110d49296e9cd4
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-02 20:43:04 -0300

    refactor: Removido construtor e atributos não mais utilizados.

--- ContaCorrente.ConsoleApp/Entities/AccountMovement.cs
+++ ContaCorrente.ConsoleApp/Entities/AccountMovement.cs
@@ -4,17 +4,10 @@ public class AccountMovement
 {
     public decimal Amount;
     public string Type;
-    public CurrentAccount SourceAccount;
-    public CurrentAccount DestinationAccount;
 
     public AccountMovement(decimal amount, string type)
     {
         Amount = amount;
         Type = type;
-    }
-    public AccountMovement(decimal amount, string type, CurrentAccount sourceAccount, CurrentAccount destinationAccount) : this(amount, type)
-    {
-        SourceAccount = sourceAccount;
-        DestinationAccount = destinationAccount;
     }
 }
