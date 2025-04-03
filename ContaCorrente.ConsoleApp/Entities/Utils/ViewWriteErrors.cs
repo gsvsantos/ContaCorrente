d8da04b248d0b4275cb29b9e21edee09bbff257c
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-03 01:30:07 -0300

    feat: Adicionado métodos para fazer impressões de mensagens de erros no console.

--- ContaCorrente.ConsoleApp/Entities/Utils/ViewWriteErrors.cs
+++ ContaCorrente.ConsoleApp/Entities/Utils/ViewWriteErrors.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace ContaCorrente.ConsoleApp.Entities.Utils;
 
-namespace ContaCorrente.ConsoleApp.Entities.Utils
+public class ViewWriteErrors
 {
-    internal class ViewWriteErrors
+    public static void HitMaxTransactionLimit()
     {
+        Console.WriteLine("Você atingiu o limite de transações!");
+    }
+    public static void InsuficientBalanceToTransfer()
+    {
+        Console.WriteLine("Seu saldo é insuficiente para essa transferência!");
+    }
+    public static void ValueToTransferNeedsToBePositive()
+    {
+        Console.WriteLine("O valor de transferência precisa ser positivo.");
+    }
+    public static void ValueToDepositNeedsToBePositive()
+    {
+        Console.WriteLine("O valor para depósito precisa ser positivo.");
+    }
+    public static void InsuficientBalanceToWithdraw()
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public static void ValueToWithdrawNeedsToBePositive()
+    {
+        Console.WriteLine("O valor do saque precisa ser positivo.");
+    }
+    public static void TransactionHistoryIsEmpty()
+    {
+        Console.WriteLine("Ainda não foi feito nenhum movimento nesta conta!");
     }
 }
