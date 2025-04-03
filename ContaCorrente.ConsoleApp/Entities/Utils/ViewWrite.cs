6c64a32894a420aa675a247ba2d3e90c532ab340
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-03 01:29:41 -0300

    feat: Adicionado métodos para melhor organização das impressões de mensagens no console.

--- ContaCorrente.ConsoleApp/Entities/Utils/ViewWrite.cs
+++ ContaCorrente.ConsoleApp/Entities/Utils/ViewWrite.cs
@@ -1,5 +1,26 @@
 ﻿namespace ContaCorrente.ConsoleApp.Entities.Utils;
 
-internal class ViewWrite
+public class ViewWrite
 {
+    public static void ShowExtractHeader(CurrentAccount account)
+    {
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        Console.WriteLine($"Extrato da Conta #{account.AccountNumber}");
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+    }
+    public static void ActualBalanceHeader(decimal actualBalance)
+    {
+        Console.WriteLine("\n---------------------------------------------");
+        Console.WriteLine($"Saldo atual: R$ {actualBalance:F2}");
+        Console.WriteLine("---------------------------------------------\n");
+    }
+    public static void AccountMovementMessage()
+    {
+        Console.WriteLine("Movimentações:");
+        Console.WriteLine("---------------------------------------------");
+    }
+    public static void TransactionsMessage(string type, decimal amount)
+    {
+        Console.WriteLine($"{type} de R$ {amount:F2}");
+    }
 }
