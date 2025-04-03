ebe1df0a17e6dd3a8180ec6a5e39b4da53f16991
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-02 23:53:07 -0300

    refactor: Aprimorada instanciação das contas para melhor organização do código.

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -6,24 +6,13 @@ internal class Program
 {
     static void Main(string[] args)
     {
-        CurrentAccount account1 = new CurrentAccount();
-
-        account1.AvailableBalance = 1000;
-        account1.AccountNumber = 12;
-        account1.OverdraftLimit = 0;
-        account1.Transaction = new AccountMovement[10];
+        CurrentAccount account1 = new CurrentAccount(12, 1000, 0);
+        CurrentAccount account2 = new CurrentAccount(13, 300, 0);
 
         account1.Withdraw(200);
         account1.Deposit(300);
         account1.Deposit(500);
         account1.Withdraw(200);
-
-        CurrentAccount account2 = new CurrentAccount();
-
-        account2.AvailableBalance = 300;
-        account2.AccountNumber = 13;
-        account2.OverdraftLimit = 0;
-        account2.Transaction = new AccountMovement[10];
 
         account1.TransferTo(account2, 400);
         Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
