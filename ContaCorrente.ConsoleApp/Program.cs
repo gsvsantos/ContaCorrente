ab17c4c6918898bf3fd84363da13ccd50073932a
gsvsantos <gustavocnsantos02@gmail.com>
2025-04-02 23:14:49 -0300

    feat: Inicializada primeira versão do `Program`.

--- ContaCorrente.ConsoleApp/Program.cs
+++ ContaCorrente.ConsoleApp/Program.cs
@@ -1,9 +1,38 @@
-﻿namespace ContaCorrente.ConsoleApp;
+﻿using ContaCorrente.ConsoleApp.Entities;
+
+namespace ContaCorrente.ConsoleApp;
 
 internal class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        CurrentAccount account1 = new CurrentAccount();
+
+        account1.AvailableBalance = 1000;
+        account1.AccountNumber = 12;
+        account1.OverdraftLimit = 0;
+        account1.Transaction = new AccountMovement[10];
+
+        account1.Withdraw(200);
+        account1.Deposit(300);
+        account1.Deposit(500);
+        account1.Withdraw(200);
+
+        CurrentAccount account2 = new CurrentAccount();
+
+        account2.AvailableBalance = 300;
+        account2.AccountNumber = 13;
+        account2.OverdraftLimit = 0;
+        account2.Transaction = new AccountMovement[10];
+
+        account1.TransferTo(account2, 400);
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        Console.WriteLine($"Extrato da Conta #{account1.AccountNumber}");
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        account1.ShowStatement();
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        Console.WriteLine($"Extrato da Conta #{account2.AccountNumber}");
+        Console.WriteLine("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=");
+        account2.ShowStatement();
     }
 }
